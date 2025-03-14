9288783127a0fb86ac9354aa57cc460c17370c97
Palleti-Varshita <p.varshita1105@gmail.com>
2025-03-14 11:03:14 +0530

    made navbar layout
    Signed-off-by: Palleti-Varshita <p.varshita1105@gmail.com>
    
    Side navbar layout created

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,82 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+export default function Home() 
+{
+    return(
+        <div>
+     <div className="sidebar">
+      <img src="public/images/gitam.png"></img>
+             <details>
+            <summary>SEM 1</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 2</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 3</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 4</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 5</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 6</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 7</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
+        <details>
+            <summary>SEM 8</summary>
+            <div className="submenu">
+                <a href="#">SUBJECt1</a>
+                <a href="#">subject2</a>
+                <a href="#">subject3</a>
+            </div>
+        </details>
     </div>
-  );
+    <div className="content">
+        <h2>Welcome to CHITTI !</h2>
+        <p>YOUR PARTNER IN CRIME BUAHAHAHAH !!</p>
+    </div>
+        </div>
+    );
+   
 }
---
+++ src/app/welcome.js
@@ -0,0 +1,16 @@
+export default function Welcome() {
+    return (
+        <div>
+    <button class="open-btn" onclick="toggleSidebar()">☰ Open Sidebar</button>
+
+    <div class="sidebar" id="myside">
+        <a href="#"> Close</a>
+        <a href="#">SEM 1</a>
+        
+    </div>
+
+    
+
+        </div>
+    );
+}
