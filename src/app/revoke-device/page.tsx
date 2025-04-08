fcfeb92410545349049dd543bc62f7cf22a47a4a
Manas Malla <manasmalla.dev@gmail.com>
2025-04-08 09:14:02 +0530

    added eslint
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/revoke-device/page.tsx
+++ src/app/revoke-device/page.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable  @typescript-eslint/no-explicit-any */
+/* eslint-disable  @typescript-eslint/no-unused-vars */
 // app/admin/revoke-device/page.tsx
 "use client";
 import React, { useState } from "react";
