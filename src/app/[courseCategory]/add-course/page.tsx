ffa288de5b15170e87859ac97990c42d41067da8
Manas Malla <manasmalla.dev@gmail.com>
2025-08-24 14:34:08 +0530

    eslint

--- src/app/[courseCategory]/add-course/page.tsx
+++ src/app/[courseCategory]/add-course/page.tsx
@@ -1,4 +1,5 @@
 /* eslint-disable  @typescript-eslint/no-explicit-any */
+
 "use client";
 import React, { useState, useEffect, useRef } from "react";
 import styles from "./AddCourseForm.module.css";
